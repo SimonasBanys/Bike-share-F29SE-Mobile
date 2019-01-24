858fa2eb04751969eac248e91000c28bd61b2581
Regan Hay <regs.d.h@gmail.com>
2019-01-24 14:57:08 +0000

    Pseudocode Added 2 New Tables

--- createTables.sql
+++ createTables.sql
@@ -2,65 +2,138 @@ DROP TABLE test;
 
 CREATE TABLE userInfo
     --this table contains unique user login information
-    test INT PRIMARY KEY AUTO_INCREMENT -- in reality this would be generated number not auto inc.
+    userID INT PRIMARY KEY AUTO_INCREMENT, -- in reality this would be generated number not auto inc.
+    username,
+    firstName,
+    lastName,
+    passwordHash,
+    salt,
+    email,
+    DoB
 )ENGINE=INNODB;
 
 CREATE TABLE bikeInfo (
     --this table contains all unique bike identifiers
-    test INT PRIMARY KEY AUTO_INCREMENT -- in reality this would be generated number not auto inc.
+    bikeID INT PRIMARY KEY AUTO_INCREMENT, -- in reality this would be generated number not auto inc.
+    inEco bool
+
 )ENGINE=INNODB;
 
 CREATE TABLE managementInfo (
   --this table contains unique management login information
-    test INT PRIMARY KEY AUTO_INCREMENT -- in reality this would be generated number not auto inc.
+    staffID INT PRIMARY KEY AUTO_INCREMENT, -- in reality this would be generated number not auto inc.
+    username,
+    firstName,
+    lastName,
+    passwordHash,
+    salt,
+    email,
+    DoB,
+    jobTitle enum,
+    workingHours
 )ENGINE=INNODB;
 
 CREATE TABLE stationInfo (
     --this table contains all the stations. used for mapping
-    test INT PRIMARY KEY AUTO_INCREMENT -- in reality this would be generated number not auto inc.
+    stationID INT PRIMARY KEY AUTO_INCREMENT, -- in reality this would be generated number not auto inc.
+    stationName,
+    latitude,
+    longitude,
+    addressLine1,
+    addressLine2, --can be NULL
+    postcode
+    --example for Edinburgh is lat 55.950161, long -3.213177
+
+)ENGINE=INNODB;
+
+CREATE TABLE stationStatus (
+  --one to one relationship with stationInfo. Therefore primary key is forgeign key
+    stationID primary/forgeign, -- in reality this would be generated number not auto inc.
+    maxParkingSpaces,
+    availableParkingSpaces
 )ENGINE=INNODB;
 
 CREATE TABLE bikesAtStations (
+    bikeID,
+    stationID,
+    PRIMARY KEY (bikeID, stationID),
     --forgeign keys : bike ID @ station ID
-    test INT PRIMARY KEY AUTO_INCREMENT -- in reality this would be generated number not auto inc.
+    FOREIGN KEY (bikeID) REFERENCES bikeInfo (bikeID),
+    FOREIGN KEY (stationID) REFERENCES stationStatus (stationID)
 )ENGINE=INNODB;
 
 CREATE TABLE usersUsingBikes (
     --current bikes that are being used by users
-    test INT PRIMARY KEY AUTO_INCREMENT -- in reality this would be generated number not auto inc.
-)ENGINE=INNODB;
+    PRIMARY KEY (bikeID, userID),
+    --forgeign keys : bike ID @ station ID
+    FOREIGN KEY (bikeID) REFERENCES bikeInfo (bikeID),
+    FOREIGN KEY (userID) REFERENCES userInfo (userID)
 
 CREATE TABLE bikeTracking (
-    --combination of bikesAtStatiosn and usersUsingBikes
-    test INT PRIMARY KEY AUTO_INCREMENT -- in reality this would be generated number not auto inc.
+    --combination of bikesAtStations and usersUsingBikes
+    test INT PRIMARY KEY AUTO_INCREMENT, -- in reality this would be generated number not auto inc.
 )ENGINE=INNODB;
 
 CREATE TABLE finishedRides (
     --old logs of all the finished rides
-    test INT PRIMARY KEY AUTO_INCREMENT -- in reality this would be generated number not auto inc.
+    rideID INT PRIMARY KEY AUTO_INCREMENT, -- in reality this would be generated number not auto inc.
+    userID,
+    startStationID,
+    endStationID,
+    rideLength,
+    pricePaid
 )ENGINE=INNODB;
 
 CREATE TABLE scheduledMaintenance (
     --management scheduled maintenance
-    test INT PRIMARY KEY AUTO_INCREMENT -- in reality this would be generated number not auto inc.
+    maintenanceID INT PRIMARY KEY AUTO_INCREMENT, -- in reality this would be generated number not auto inc.
+    bikeID,
+    staffID, --who scheduled the maintenance
+    dateScheduled,
+    estimatedLengthOfRepair
+
 )ENGINE=INNODB;
 
 CREATE TABLE completedMaintenance (
     --old logs of all completed maintenance
-    test INT PRIMARY KEY AUTO_INCREMENT -- in reality this would be generated number not auto inc.
+    maintenanceID INT PRIMARY KEY AUTO_INCREMENT, -- in reality this would be generated number not auto inc.
+    bikeID,
+    staffID, --who fixed the bike
+    dateScheduled,
+    estimatedLengthOfRepair,
+    dateOfCompletion,
+    notes
 )ENGINE=INNODB;
 
 CREATE TABLE currentBookings (
     --current bookings that have not been completed yet
-    test INT PRIMARY KEY AUTO_INCREMENT -- in reality this would be generated number not auto inc.
+    bookingID INT PRIMARY KEY AUTO_INCREMENT, -- in reality this would be generated number not auto inc.
+    bikeID,
+    stationID,
+    userID,
+    timeOfBooking
 )ENGINE=INNODB;
 
 CREATE TABLE oldBookings (
     --old logs for bookings
-    test INT PRIMARY KEY AUTO_INCREMENT -- in reality this would be generated number not auto inc.
+    bookingID INT PRIMARY KEY AUTO_INCREMENT, -- in reality this would be generated number not auto inc.
+    bikeID,
+    stationID,
+    userID,
+    timeOfBooking,
+    dateOfBooking,
+    pickedUp, --bool
+    extraCharge --bool
 )ENGINE=INNODB;
 
 CREATE TABLE bikeData (
     --fancy stats
-    test INT PRIMARY KEY AUTO_INCREMENT -- in reality this would be generated number not auto inc.
+    bikeID INT PRIMARY KEY AUTO_INCREMENT, -- in reality this would be generated number not auto inc.
+
+)ENGINE=INNODB;
+
+CREATE TABLE userData (
+    --fancy stats
+    userID INT PRIMARY KEY AUTO_INCREMENT, -- in reality this would be generated number not auto inc.
+
 )ENGINE=INNODB;
