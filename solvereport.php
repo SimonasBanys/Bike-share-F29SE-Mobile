77375697d0cfa172c4985d2c1331949f3ecf386f
Regan Hay <regs.d.h@gmail.com>
2019-02-05 16:55:25 +0000

    solvereport.php done

--- solvereport.php
+++ solvereport.php
@@ -1,3 +1,7 @@
+<html>
+<body>
+<h1>Solving report...</h1>
+
 <?php
 DEFINE ('DB_HOST', 'mysql-server-1.macs.hw.ac.uk');
 DEFINE ('DB_USER', 'rh49');
@@ -8,20 +12,55 @@ $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 OR die('Could not connect to MySQL Database: ' . mysqli_connect_error());
 
 $array = Array();
+$repID = $_POST[reportID];
 
-$result = mysql_query("SELECT * FROM CurrentUserReports WHERE reportID = '$_POST[reportID]'");
-while ($row = mysql_fetch_array($result)) {
+echo "Array built... <br>";
+
+$query = "SELECT * FROM CurrentUserReports WHERE reportID ='" .$repID. "'";
+echo "query built... <br>";
+$result = mysqli_query($dbc, $query);
+echo "got result... <br>";
+while($row = mysqli_fetch_array($result)) {
     $array[] = $row;
 }
 
+$v1 = $array[0][1];
+$v2 = $array[0][2];
+$v3 = $array[0][3];
+$v4 = $array[0][4];
+$v5 = $array[0][5];
+$staffID = $_POST[staffID];
+$nM = $_POST[needsMaintenance];
+
 $query="INSERT INTO SolvedUserReports
 (reportID, userID, bikeID, problem, latitude, longitude, staffID, needsMaintenance)
-VALUES ('$_POST[reportID]',. $array[1] .,. $array[2] .,. $array[3] .,. $array[4] .,. $array[5] .,'$_POST[staffID]','$_POST[needsMaintenance]')";
+VALUES ($repID,$v1,$v2,'$v3',$v4,$v5,$staffID,'$nM')";
 
 if(mysqli_query($dbc, $query)){
-    echo "";
+
+    echo "Done <br>";
+
+    echo "Deleting old report... <br>";
+
+    $delQuery = "DELETE FROM CurrentUserReports WHERE reportID ='" .$repID. "'";
+
+    if(mysqli_query($dbc, $delQuery)){
+        echo "Done <br>";
+    }else{
+        echo "ERROR:" . mysqli_error($dbc);
+    }
+
 }else{
     echo "ERROR:" . mysqli_error($dbc);
 }
 
+
 ?>
+<form method ="post" action="report.php">
+    <h1>
+        Press to go back
+    </h1>
+    <button type="submit">BACK</button>
+</form>
+</body>
+</html>
