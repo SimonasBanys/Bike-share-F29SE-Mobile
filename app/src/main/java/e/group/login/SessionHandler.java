1f56efd880f836a3fa92de1b43e14dee0abfb207
Perkuns <31985858+Perkuns@users.noreply.github.com>
2019-02-06 11:12:46 +0000

    login, register, and map screens
    screens are working, does not create user nor authenticate login

---
+++ app/src/main/java/e/group/login/SessionHandler.java
@@ -0,0 +1,95 @@
+package e.group.login;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.Date;
+
+public class SessionHandler {
+    private static final String PREF_NAME = "UserSession";
+    private static final String KEY_USERNAME = "username";
+    private static final String KEY_EXPIRES = "expires";
+    private static final String KEY_FIRST_NAME = "first_name";
+    private static final String KEY_LAST_NAME = "last_name";
+    private static final String KEY_EMPTY = "";
+    private Context mContext;
+    private SharedPreferences.Editor mEditor;
+    private SharedPreferences mPreferences;
+
+    public SessionHandler(Context mContext) {
+        this.mContext = mContext;
+        mPreferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        this.mEditor = mPreferences.edit();
+    }
+
+    /**
+     * Logs in the user by saving user details and setting session
+     *
+     * @param username
+     * @param firstName
+     * @param lastName
+     */
+    public void loginUser(String username, String firstName, String lastName) {
+        mEditor.putString(KEY_USERNAME, username);
+        mEditor.putString(KEY_FIRST_NAME, firstName);
+        mEditor.putString(KEY_LAST_NAME, lastName);
+
+        Date date = new Date();
+
+        //Set user session for next 7 days
+        long millis = date.getTime() + (7 * 24 * 60 * 60 * 1000);
+        mEditor.putLong(KEY_EXPIRES, millis);
+        mEditor.commit();
+    }
+
+    /**
+     * Checks whether user is logged in
+     *
+     * @return
+     */
+    public boolean isLoggedIn() {
+        Date currentDate = new Date();
+
+        long millis = mPreferences.getLong(KEY_EXPIRES, 0);
+
+        /* If shared preferences does not have a value
+         then user is not logged in
+         */
+        if (millis == 0) {
+            return false;
+        }
+        Date expiryDate = new Date(millis);
+
+        /* Check if session is expired by comparing
+        current date and Session expiry date
+        */
+        return currentDate.before(expiryDate);
+    }
+
+    /**
+     * Fetches and returns user details
+     *
+     * @return user details
+     */
+    public User getUserDetails() {
+        //Check if user is logged in first
+        if (!isLoggedIn()) {
+            return null;
+        }
+        User user = new User();
+        user.setUsername(mPreferences.getString(KEY_USERNAME, KEY_EMPTY));
+        user.setFirstName(mPreferences.getString(KEY_FIRST_NAME, KEY_EMPTY));
+        user.setLastName(mPreferences.getString(KEY_LAST_NAME, KEY_EMPTY));
+
+        return user;
+    }
+
+    /**
+     * Logs out user by clearing the session
+     */
+    public void logoutUser(){
+        mEditor.clear();
+        mEditor.commit();
+    }
+
+}
